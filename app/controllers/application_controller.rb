0b70f56b1369419817222e490bcaea21f09b7275
Ryan Gilboy <rgilboy@chicagobooth.edu>
2023-04-09 22:31:32 +0000

    square_calcs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::Base
     render({:template =>  "calculation_templates/square_form.html.erb"})
   end
 
+
+  # params = {"number"=>"42"}
+  def calculate_square
+    @num = params.fetch("number").to_i
+    @square_of_num = @num * @num
+    render({:template => "calculation_templates/square_results.html.erb"})
+  end
+
 end
