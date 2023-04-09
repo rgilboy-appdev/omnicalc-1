2efe80235ac768e2d7953954777133c1720e6fed
Ryan Gilboy <rgilboy@chicagobooth.edu>
2023-04-09 23:51:09 +0000

    Got it working up to sqrt

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,22 @@ class ApplicationController < ActionController::Base
 
   # params = {"number"=>"42"}
   def calculate_square
-    @num = params.fetch("number").to_i
+    @num = params.fetch("number").to_f
     @square_of_num = @num * @num
     render({:template => "calculation_templates/square_results.html.erb"})
   end
+
+  def blank_root_form
+    render({:template => "calculation_templates/square_root_form.html.erb"})
+  end
+
+  def calculate_root
+
+    @num = params.fetch("number").to_f
+    @sqrt_of_num = @num ** 0.5
+    render({:template => "calculation_templates/square_root_results.html.erb"})
+
+  end
+
 
 end
